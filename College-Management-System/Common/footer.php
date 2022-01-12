f019793496339c02ce502f74d488789c64b6c3f2
rayyanabubakr <71322861+rayyanabubakr@users.noreply.github.com>
2022-01-12 12:16:33 +0530

    Add files via upload

---
+++ College-Management-System/Common/footer.php
@@ -0,0 +1,40 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Newton University</h3>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About NU</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B.Sc. Architecture</a></li>
+              <li><a href="">B.Sc. Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Newton University,
+                  washington, USA </p>
+              <p>Telephones: (+1) 35978525-32</p>
+              <p>E-mail: admissions@nu.edu.us</p>
+        </div>
+      </div>
+    </div>
+    </div>
+
+</footer>
